2f1c3f9b1baff7733aeadc21ab6d7086ad3307ec
ayleen5 <ayleenm55@gmail.com>
2023-06-10 17:03:09 +0300

    trying to merge

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Question.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Question.java
@@ -10,18 +10,6 @@ public class Question implements Serializable {
     private int correct;
     private String subject;
     private int points;
-
-    public Question(){}
-    public Question(String id, String text,String[] answers, int correct, String subject, int points){
-        this.id = id;
-        this.text = text;
-        this.answers = new String[4];
-        for(int i = 0; i< 4; i++)
-            (this.answers)[i] = answers[i];
-        this.correct = correct;
-        this.subject = subject;
-        this.points = points;
-    }
     public String getId() {
         return id;
     }
