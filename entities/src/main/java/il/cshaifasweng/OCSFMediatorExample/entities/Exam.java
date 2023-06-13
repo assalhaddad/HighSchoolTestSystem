9c16cd23787c4fb85a5142b06412f138ef3e4d25
shadysaf <43337159+shadysaf@users.noreply.github.com>
2023-06-13 13:56:18 +0300

    subjectList is working

--- entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Exam.java
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/Exam.java
@@ -1,10 +1,16 @@
 package il.cshaifasweng.OCSFMediatorExample.entities;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Exam implements Serializable {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String id_exam;
     private ArrayList<Question> questions;
     private int time;
     private String freeTextStudent;
@@ -12,4 +18,87 @@ public class Exam implements Serializable {
     private String author;
     private int moreTime;
     private ArrayList<Integer> correctAnswers;
+
+    public Exam(String id_exam, ArrayList<Question> questions, int time, String freeTextStudent, String freeTextTeacher, String author, int moreTime, ArrayList<Integer> correctAnswers) {
+        this.id_exam = id_exam;
+        this.questions = questions;
+        this.time = time;
+        this.freeTextStudent = freeTextStudent;
+        this.freeTextTeacher = freeTextTeacher;
+        this.author = author;
+        this.moreTime = moreTime;
+        this.correctAnswers = correctAnswers;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getId_exam() {
+        return id_exam;
+    }
+
+    public void setId_exam(String id_exam) {
+        this.id_exam = id_exam;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getFreeTextStudent() {
+        return freeTextStudent;
+    }
+
+    public void setFreeTextStudent(String freeTextStudent) {
+        this.freeTextStudent = freeTextStudent;
+    }
+
+    public String getFreeTextTeacher() {
+        return freeTextTeacher;
+    }
+
+    public void setFreeTextTeacher(String freeTextTeacher) {
+        this.freeTextTeacher = freeTextTeacher;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getMoreTime() {
+        return moreTime;
+    }
+
+    public void setMoreTime(int moreTime) {
+        this.moreTime = moreTime;
+    }
+
+    public ArrayList<Integer> getCorrectAnswers() {
+        return correctAnswers;
+    }
+
+    public void setCorrectAnswers(ArrayList<Integer> correctAnswers) {
+        this.correctAnswers = correctAnswers;
+    }
 }
